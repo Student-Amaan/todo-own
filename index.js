a66918209264a8506eb8954a700a3a7d4d1c2b69
waxir <danishdarman06@gmail.com>
2026-03-13 21:11:52 +0500

    js part commited

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ const ulList = document.getElementById("list-item");
 
 let tasks = []
 
+addTask.addEventListener('click', () => {
+    const inputText = inputField.value.trim();
+    if(inputField === "") return;
+    const newtask = {
+        id: Date.now(),
+        text: inputText,
+        completed: false
+    }
+
+    tasks.push(newtask)
+    inputField.value = ""
+    console.log(tasks)
+})
+
+function saveTask(){
+    localStorage.setItem()
+}
+
