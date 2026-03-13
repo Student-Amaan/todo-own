b1b9dc0d5f1312c012a7393213a0564d327b5667
waxir <danishdarman06@gmail.com>
2026-03-13 20:58:35 +0500

    todo is commited

---
+++ index.js
@@ -0,0 +1,6 @@
+const inputField= document.getElementById("input-field");
+const addTask = document.getElementById("add-task");
+const ulList = document.getElementById("list-item");
+
+let tasks = []
+
