fe3e059e7b1bd1c24c98d0b2cc5099b0b3e2da0a
waxir <danishdarman06@gmail.com>
2026-03-14 21:36:35 +0500

    event bubbling and crud operation is commited

--- index.js
+++ index.js
@@ -1,47 +1,52 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-    const inputField = document.getElementById("input-field");
-    const addTask = document.getElementById("add-task");
-    const ulList = document.getElementById("list-item");
+document.addEventListener("DOMContentLoaded", () => {
+  const inputField = document.getElementById("input-field");
+  const addTask = document.getElementById("add-task");
+  const ulList = document.getElementById("list-item");
 
-    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-    tasks.forEach((task) => randerTask(task))
+  tasks.forEach((task) => randerTask(task));
 
-    addTask.addEventListener("click", () => {
-      const inputText = inputField.value.trim();
-      if (inputField === "") return;
-      const newtask = {
-        id: Date.now(),
-        text: inputText,
-        completed: false,
-      };
+  addTask.addEventListener("click", () => {
+    const inputText = inputField.value.trim();
+    if (inputField === "") return;
+    const newtask = {
+      id: Date.now(),
+      text: inputText,
+      completed: false,
+    };
 
-      tasks.push(newtask);
-      saveTask()
-      inputField.value = "";
-      console.log(tasks);
-    });
+    tasks.push(newtask);
+    saveTask();
+    randerTask(newtask);
+    inputField.value = "";
+    console.log(tasks);
+  });
 
-    function randerTask(task) {
-      let li = document.createElement('li')
-      if(task.completed == li.classList.add('completed'))
-      li.innerHTML = `<span>${task.id}</span>
+  function randerTask(task) {
+    let li = document.createElement("li");
+    li.setAttribute("data_id", task.id);
+    if (task.completed) li.classList.add("completed");
+    li.innerHTML = `<span>${task.text}</span>
       <button>deleted</button>
-      `
-      ulList.appendChild(li)
-      saveTask()
+      `;
+    ulList.appendChild(li);
 
-      li.addEventListener('click',(e)=>{
-        if(e.target.tagName === "BUTTON" ) return;
-        task.completed = !task.completed
-        li.classList.toggle('completed')
-        saveTask()
-      })
-    }
+    li.addEventListener("click", (e) => {
+      if (e.target.tagName === "BUTTON") return;
+      task.completed = !task.completed;
+      li.classList.toggle("completed");
+      saveTask();
+    });
+    li.querySelector("button").addEventListener("click", (e) => {
+      e.stopPropagation();
+      tasks = tasks.filter((t) => t.id !== task.id);
+      li.remove();
+      saveTask();
+    });
+  }
 
-    function saveTask() {
-      localStorage.setItem("tasks", JSON.stringify(tasks));
-    }
-
-
+  function saveTask() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
 })
