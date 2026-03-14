a0a029ea573f1b180189e309e2fc08e0c22ea2e7
waxir <danishdarman06@gmail.com>
2026-03-14 21:09:14 +0500

    event bubling is commited'

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const addTask = document.getElementById("add-task");
     const ulList = document.getElementById("list-item");
 
-    let tasks = JSON.parse(localStorage.getItem(task)) || [];
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    tasks.forEach((task) => randerTask(task))
 
     addTask.addEventListener("click", () => {
       const inputText = inputField.value.trim();
@@ -15,12 +17,26 @@ document.addEventListener('DOMContentLoaded', ()=>{
       };
 
       tasks.push(newtask);
+      saveTask()
       inputField.value = "";
       console.log(tasks);
     });
 
     function randerTask(task) {
-      console.log(task);
+      let li = document.createElement('li')
+      if(task.completed == li.classList.add('completed'))
+      li.innerHTML = `<span>${task.id}</span>
+      <button>deleted</button>
+      `
+      ulList.appendChild(li)
+      saveTask()
+
+      li.addEventListener('click',(e)=>{
+        if(e.target.tagName === "BUTTON" ) return;
+        task.completed = !task.completed
+        li.classList.toggle('completed')
+        saveTask()
+      })
     }
 
     function saveTask() {
