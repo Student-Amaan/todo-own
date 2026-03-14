9edc8abab4c1f265ff0b621c09a6ed671b6a4b14
waxir <danishdarman06@gmail.com>
2026-03-14 20:38:55 +0500

    domContentLoaded and localStorage is commited

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-const inputField= document.getElementById("input-field");
-const addTask = document.getElementById("add-task");
-const ulList = document.getElementById("list-item");
+document.addEventListener('DOMContentLoaded', ()=>{
+    const inputField = document.getElementById("input-field");
+    const addTask = document.getElementById("add-task");
+    const ulList = document.getElementById("list-item");
 
-let tasks = []
+    let tasks = JSON.parse(localStorage.getItem(task)) || [];
 
-addTask.addEventListener('click', () => {
-    const inputText = inputField.value.trim();
-    if(inputField === "") return;
-    const newtask = {
+    addTask.addEventListener("click", () => {
+      const inputText = inputField.value.trim();
+      if (inputField === "") return;
+      const newtask = {
         id: Date.now(),
         text: inputText,
-        completed: false
+        completed: false,
+      };
+
+      tasks.push(newtask);
+      inputField.value = "";
+      console.log(tasks);
+    });
+
+    function randerTask(task) {
+      console.log(task);
     }
 
-    tasks.push(newtask)
-    inputField.value = ""
-    console.log(tasks)
+    function saveTask() {
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+
+
 })
-
-function saveTask(){
-    localStorage.setItem()
-}
-
